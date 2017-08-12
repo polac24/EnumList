802444c565faca8a1999f5254d4282e45512a223
Bartosz Polaczyk <polac24@gmail.com>
2017-08-12 12:24:27 +0200

    Add unit tests

--- Project/EnumListProjectTests/EnumListProjectTests.swift
+++ Project/EnumListProjectTests/EnumListProjectTests.swift
@@ -7,30 +7,136 @@
 //
 
 import XCTest
-@testable import EnumListProject
+import EnumList
 
 class EnumListProjectTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    private enum SubjectString: EnumListRawRepresentable<SubjectString.Values>, RawRepresentable{
+        struct Values:EnumValues {
+            typealias Element = SubjectString
+            
+            static var allRaws:Set<String> = []
         }
+        
+        case caseNo1 = "case1"
+        case caseNo2 = "case2"
     }
+    
+    private enum SubjectInt: EnumListRawRepresentable<SubjectInt.Values>, RawRepresentable{
+        struct Values:EnumValues {
+            typealias Element = SubjectInt
+            
+            static var allRaws:Set<Int> = []
+        }
+        
+        case caseNo1 = 1
+        case caseNo2 = 200
+    }
+    
+    
+    func testAllStringEnumsExistInAllList(){
+        // Arrange
+        
+        // Act
+        let allValues = SubjectString.Values.all()
+        // Assert
+        XCTAssertEqual(allValues, Set([.caseNo1, .caseNo2]))
+    }
+    
+    func testAllRawStringValuesExistInAllRaws(){
+        // Arrange
+        
+        // Act
+        _ = SubjectString.Values.all()
+        let allRaws = SubjectString.Values.allRaws
+        // Assert
+        XCTAssertEqual(allRaws, Set(["case1", "case2"]))
+    }
+    
+    func testNotCallingAllBeforeAskingForAllRawsReturnsEmptySet(){
+        enum PrivateSubject: EnumListRawRepresentable<PrivateSubject.Values>, RawRepresentable{
+            struct Values:EnumValues {
+                typealias Element = PrivateSubject
+                
+                static var allRaws:Set<String> = []
+            }
+            
+            case caseNo1 = "case1"
+            case caseNo2 = "case2"
+        }
+        
+        // Arrange
+        
+        // Act
+        let allRaws = PrivateSubject.Values.allRaws
+        // Assert
+        XCTAssertTrue(allRaws.isEmpty)
+    }
+    
+    func testFetchingNonExistingEnumFillsAllRawsSet(){
+        enum PrivateSubject: EnumListRawRepresentable<PrivateSubject.Values>, RawRepresentable{
+            struct Values:EnumValues {
+                typealias Element = PrivateSubject
+                
+                static var allRaws:Set<String> = []
+            }
+            
+            case caseNo1 = "case1"
+            case caseNo2 = "case2"
+        }
+        
+        // Arrange
+        
+        // Act
+        _ = PrivateSubject(rawValue: EnumListRawRepresentable<PrivateSubject.Values> ())
+        
+        // Assert
+        let allRaws = PrivateSubject.Values.allRaws
+        XCTAssertFalse(allRaws.isEmpty)
+    }
+    
+    func testAskingForAllEnumsFillsAllRawsSet(){
+        enum PrivateSubject: EnumListRawRepresentable<PrivateSubject.Values>, RawRepresentable{
+            struct Values:EnumValues {
+                typealias Element = PrivateSubject
+                
+                static var allRaws:Set<String> = []
+            }
+            
+            case caseNo1 = "case1"
+            case caseNo2 = "case2"
+        }
+        
+        // Arrange
+        
+        // Act
+        _ = PrivateSubject.Values.all()
+        
+        // Assert
+        let allRaws = PrivateSubject.Values.allRaws
+        XCTAssertFalse(allRaws.isEmpty)
+    }
+    
+    
+    func testAllIntEnumsExistInAllList(){
+        // Arrange
+        
+        // Act
+        let allValues = SubjectInt.Values.all()
+        // Assert
+        XCTAssertEqual(allValues, Set([.caseNo1, .caseNo2]))
+    }
+    
+    func testAllRawIntValuesExistInAllRaws(){
+        // Arrange
+        
+        // Act
+        _ = SubjectInt.Values.all()
+        let allRaws = SubjectInt.Values.allRaws
+        // Assert
+        XCTAssertEqual(allRaws, Set([1, 200]))
+    }
+    
+   
     
 }
