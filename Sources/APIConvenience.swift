9918dd50b48db3eebdfb513ad921b574695dacfe
Bartosz Polaczyk <polac24@gmail.com>
2017-09-13 19:36:46 +0200

    Fix swift4 for xcode9 warnings

--- Sources/APIConvenience.swift
+++ Sources/APIConvenience.swift
@@ -10,12 +10,12 @@ import Foundation
 
 
 public extension RawRepresentable{
-    init?<T>(raw rawValue: String) where RawValue == EnumListStringRaw<T>, T:StringEnumValues{
+    init?<T>(raw rawValue: String) where RawValue == EnumListStringRaw<T>{
         self.init(rawValue: RawValue(stringLiteral: rawValue))
     }
 }
 public extension RawRepresentable{
-    init?<T>(raw rawValue: Int)  where RawValue == EnumListIntRaw<T>, T:IntEnumValues{
+    init?<T>(raw rawValue: Int)  where RawValue == EnumListIntRaw<T>{
         self.init(rawValue: EnumListIntRaw<T>(integerLiteral: rawValue))
     }
 }
