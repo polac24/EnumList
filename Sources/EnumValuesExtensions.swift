9ef0a30ed6ba1a50324d790f3fca4f64e6f9374c
Duc Do <duc.fet01@gmail.com>
2018-03-02 11:36:10 +0200

    Make initialize function private

--- Sources/EnumValuesExtensions.swift
+++ Sources/EnumValuesExtensions.swift
@@ -29,7 +29,7 @@ public extension StringEnumValues where Element.RawValue == EnumListStringRaw<Se
         initialize()
         return allRaws
     }
-    static func initialize(){
+    private static func initialize(){
         resetRaws(imaginationRawValue: EnumListStringRaw<Self>())
     }
 }
@@ -43,7 +43,7 @@ public extension IntEnumValues where Element.RawValue == EnumListIntRaw<Self>, R
         initialize()
         return allRaws
     }
-    static func initialize(){
+    private static func initialize(){
         resetRaws(imaginationRawValue: EnumListIntRaw<Self>())
     }
 }
